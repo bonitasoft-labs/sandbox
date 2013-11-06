deb4389482e6d0f9fa801b0d5a3cb495c43cc548
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2013-11-06 15:34:53 +0100

    PersistenceUtil refactoring + in memory H2 database

--- hibernate-poc/src/main/java/org/bonitasoft/poc/util/PersistenceUtil.java
+++ hibernate-poc/src/main/java/org/bonitasoft/poc/util/PersistenceUtil.java
@@ -5,51 +5,57 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 2.0 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 package org.bonitasoft.poc.util;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 /**
  * @author Romain Bioteau
- *
  */
 public class PersistenceUtil {
 
-	private static PersistenceUtil INSTANCE;
+    private static PersistenceUtil INSTANCE;
 
-	public static synchronized PersistenceUtil getInstance(){
-		if(INSTANCE == null){
-			INSTANCE = new PersistenceUtil();
-		}
-		return INSTANCE ;
-	}
+    public static synchronized PersistenceUtil getInstance() {
+        if (INSTANCE == null) {
+            INSTANCE = new PersistenceUtil();
+        }
+        return INSTANCE;
+    }
 
-	private EntityManagerFactory entityFactory;
-	
-	PersistenceUtil(){
-		entityFactory = Persistence.createEntityManagerFactory("DefaultPersistenceUnit");
-	}
-	
-	public EntityManager createEntityManagerAndBeginTransaction() {
-		EntityManagerFactory managerFactory = getEntityManagerFactory();
-		EntityManager entityManager = managerFactory.createEntityManager();
-		entityManager.getTransaction().begin();
-		return entityManager;
-	}
+    private final EntityManagerFactory entityFactory;
 
-	private EntityManagerFactory getEntityManagerFactory() {
-		return entityFactory;
-	}
-	
+    PersistenceUtil() {
+        entityFactory = Persistence.createEntityManagerFactory("DefaultPersistenceUnit");
+    }
+
+    public void closeTransactionAndEntityManager(final EntityManager entityManager) {
+        EntityTransaction transaction = entityManager.getTransaction();
+        if (transaction.isActive()) {
+            transaction.commit();
+        }
+        entityManager.close();
+    }
+
+    public EntityManager createEntityManagerAndBeginTransaction() {
+        EntityManagerFactory managerFactory = getEntityManagerFactory();
+        EntityManager entityManager = managerFactory.createEntityManager();
+        entityManager.getTransaction().begin();
+        return entityManager;
+    }
+
+    private EntityManagerFactory getEntityManagerFactory() {
+        return entityFactory;
+    }
+
 }

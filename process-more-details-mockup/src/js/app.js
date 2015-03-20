a3d285f2359fbfa5237ef0f7fdbd4ed319342291
ntith <nicolas.tith@bonitasoft.com>
2015-03-20 14:57:44 +0100

    init the new process more details mockup

---
+++ process-more-details-mockup/src/js/app.js
@@ -0,0 +1,10 @@
+(function () {
+  'use strict';
+  var app = angular.module('seed', [
+    'ui.bootstrap',
+  ]);
+
+  app.controller('AppCtrl', ['$scope', function ($scope) {
+    $scope.greetings = 'Hello Seed';
+  }]);
+})();

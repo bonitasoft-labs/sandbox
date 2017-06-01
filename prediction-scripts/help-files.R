ca903bbf67390e1599054a58ce7891e379199263
Luis Custodio <luis.custodio@gmail.com>
2017-06-01 16:19:56 +0200

    The commands used to verify data :goat:

---
+++ prediction-scripts/help-files.R
@@ -0,0 +1,58 @@
+
+
+
+
+# Create a list of matrices
+MyList <- list(A,B,C)
+
+# Extract the 2nd column from `MyList` with the selection operator `[` with `lapply()`
+......(MyList,"[", , 2)
+
+# Extract the 1st row from `MyList`
+lapply(MyList,"[", 1, )
+
+
+apply(testRNN[c(1:2)], 1, sum)
+
+
+
+duration[2,] <- c(20, 40)
+duration <- list()
+
+duration <- matrix(nrow=9)
+duration <- list("9"="a","b",c("a","b"))
+
+duration[[4]] <- c(7,7) 
+
+
+duration[[ ActivityID[1] ]] <- c(duration[[ ActivityID[1] ]], as.numeric(difftime(testRNN$CompleteTimestamp[2],testRNN$CompleteTimestamp[2-1],units = "secs")))
+duration[[ ActivityID[1] ]] <- c(duration[[ ActivityID[1] ]], as.numeric(difftime(testRNN$CompleteTimestamp[2],testRNN$CompleteTimestamp[2-1],units = "secs")))
+
+duration <- list()
+
+duration[[8]][1] <- c(duration[[8]][1], 15)
+
+duration <- list()
+duration[[8]] <- c(15)
+duration[[8]] <- c(duration[[8]], 15)
+duration
+
+duration[[8]][1] <- c(duration[[8]][1], 15)
+
+duration[[8]] <- c(duration[[8]], 15)
+
+
+duration
+
+pg <- duration[[6]]
+
+duration[[6]] <- duration[[6]][-1]
+
+plot(density(duration[[6]]))
+summary(duration[[6]])
+
+quantile(pg, seq(0, 1, by=0.05))
+
+typeof(duration[[6]])
+
+
